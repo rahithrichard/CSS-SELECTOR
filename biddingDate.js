6915576de8bd879d751aa13500fd1ef7e130e095
rahithrichard <rahithrichard@gmail.com>
2021-01-22 15:04:48 +0530

    biddingDate 2nd commit

---
+++ biddingDate.js
@@ -0,0 +1,10 @@
+// for print css selector value
+function selectorValue() {
+
+   console.log(document.querySelector(".field-content>.lbl-licitacao:nth-of-type(6)>font").textContent);
+
+}
+// for print css selector
+function printSelector(){
+    console.log(".field-content>.lbl-licitacao:nth-of-type(6)>font");
+}
